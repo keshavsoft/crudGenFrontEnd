d3161b50bf64f4cda8e519a07bd2bbe8d9fd4499
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-05-11 12:46:19 +0530

    keshav

--- public/Garments/Stiching/VoucherShow/Js/FormLoad/StartFunc.js
+++ public/Garments/Stiching/VoucherShow/Js/FormLoad/StartFunc.js
@@ -1,6 +1,8 @@
 import { StartFunc as StartFuncAddListeners } from "./AddListeners/StartFunc.js";
+import { StartFunc as StartFuncBuildBsTable } from "./BuildBsTable/EntryFile.jsx";
 
 const StartFunc = () => {
+    StartFuncBuildBsTable();
     StartFuncAddListeners();
 };
 
