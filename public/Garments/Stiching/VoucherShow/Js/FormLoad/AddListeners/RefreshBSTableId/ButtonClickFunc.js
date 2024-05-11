824cb6dcef87344a8bd122db3ca880233917cac8
Pavan-Bhumpalli <21131a0521@gvpce.ac.in>
2024-05-11 21:30:47 +0530

    added no of items in stiching

--- public/Garments/Stiching/VoucherShow/Js/FormLoad/AddListeners/RefreshBSTableId/ButtonClickFunc.js
+++ public/Garments/Stiching/VoucherShow/Js/FormLoad/AddListeners/RefreshBSTableId/ButtonClickFunc.js
@@ -1,12 +1,29 @@
-import { StartFunc as StartFuncOnClickRowFunc } from "../onClickRow/EntryFile.js";
+import { StartFunc as StartFuncGetFetch } from "./GetFetch.js";
 
-let StartFunc = () => {
+let StartFunc = async () => {
     jFLocalHideSpinner();
+    let jVarLocalFetchData = await StartFuncGetFetch();
     var $table = $('#table');
-    $table.bootstrapTable({
-        onClickRow: StartFuncOnClickRowFunc
-    });
+    let processedData = await jFLocalInsertItemCount(jVarLocalFetchData);
+    $table.bootstrapTable("load", processedData);
+    //console.log("jVarLocalFetchData:", jVarLocalFetchData);
 };
+
+async function jFLocalInsertItemCount(inData) {
+    const response1 = await fetch('/bin/BillsStiching/dataOnly');
+    const response2 = await fetch('/bin/DeliveryStiching/dataOnly');
+    const fetchData1 = await response1.json();
+    const fetchData2 = await response2.json();
+    
+    const processedData = inData.map(item => {
+        const filteredData1 = fetchData1.filter(fetchItem => fetchItem.FK === item.pk.toString());
+        const filteredData2 = fetchData2.filter(fetchItem => fetchItem.FK === item.pk.toString());
+        item.ItemCount = filteredData1.length - filteredData2.length; // Update based on difference
+        return item;
+    });
+    
+    return processedData;
+}
 
 let jFLocalHideSpinner = () => {
     let jVarLocalSpinnerId = document.getElementById("SpinnerId");
--- public/Garments/Stiching/VoucherShow/Js/FormLoad/BuildBsTable/TableTag.js
+++ public/Garments/Stiching/VoucherShow/Js/FormLoad/BuildBsTable/TableTag.js
@@ -1,15 +1,7 @@
 const StartFunc = () => {
     var $table = $('#table');
 
-    // $table.attr("data-show-columns", "true");
-    // $table.attr("data-search", "true");
-    // $table.attr("data-search-highlight", "true");
-    // $table.attr("data-searchable", "true");
-    // $table.attr("data-show-columns-toggle-all", "true");
-    // $table.attr("data-minimum-count-columns", "2");
-    // $table.attr("data-show-pagination-switch", "true");
-    // $table.attr("data-pagination", "true");
-    $table.attr("data-toolbar", "#toolbar");
+    
     $table.attr("data-search", "true");
     $table.attr("data-searchable", "true");
     $table.attr("data-show-footer", "true");
@@ -21,7 +13,7 @@ const StartFunc = () => {
     $table.attr("data-detail-formatter", "detailFormatter");
     $table.attr("data-id-field", "pk");
     $table.attr("data-show-toggle", "true");
-    $table.attr("data-url", "/bin/StichingPOS/DataOnly");
+    //$table.attr("data-url", "/bin/StichingPOS/DataOnly");
 
 };
 
