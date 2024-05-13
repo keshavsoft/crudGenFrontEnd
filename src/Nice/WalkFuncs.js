b1438096e1d1d130f6d0239912f7b61b8ed5bc54
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-05-13 23:33:32 +0530

    keshav

---
+++ src/Nice/WalkFuncs.js
@@ -0,0 +1,32 @@
+import fs from "fs-extra";
+import nunjucks from "nunjucks";
+const results = [];
+var acutalResults = 0;
+
+var walk = function (tree, inAssetsPath) {
+    console.log("inAssetsPath : ", inAssetsPath);
+    tree.forEach(element => {
+        console.log("element : ", element);
+
+        if ("children" in element) {
+            fs.mkdirSync(`bin/${element.path}`);
+
+            walk(element.children, `../${inAssetsPath}`);
+        } else {
+            const html = fs.readFileSync(element.path, 'utf8');
+
+            let data = nunjucks.renderString(html, {
+                dir: tree,
+                inName: element.name, inAssetsPath
+            });
+
+            fs.writeFileSync(`bin/${element.path}`, data, 'utf8');
+        };
+    });
+};
+
+let CallBackFunc = (tree) => {
+    console.log("came to callback function", tree);
+};
+
+export { walk };
