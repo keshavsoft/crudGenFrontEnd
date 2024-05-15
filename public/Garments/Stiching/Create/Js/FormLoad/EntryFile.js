8631c326c69b26ae266225d0d1f7882ee089f4c8
SandhyaAvula363 <avulavenkatasandhya@gmail.com>
2024-05-15 20:04:58 +0530

    changes_made_on_stitching

--- public/Garments/Stiching/Create/Js/FormLoad/EntryFile.js
+++ public/Garments/Stiching/Create/Js/FormLoad/EntryFile.js
@@ -1,7 +1,8 @@
 import { StartFunc as StartFuncForAccountNames } from "./ForDataList/AccountNames/GetFetch.js";
-
+import {StartFunc as StartFuncForMasterItems} from "./MasterItems.js"
 const StartFunc = () => {
     StartFuncForAccountNames();
+    StartFuncForMasterItems();
 };
 
 export { StartFunc };
