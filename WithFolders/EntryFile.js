8af06bfa502770e20c9ee743e5b8f3e3fededea7
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-05-21 22:52:23 +0530

    keshav

--- WithFolders/EntryFile.js
+++ WithFolders/EntryFile.js
@@ -4,47 +4,47 @@ import fs from "fs-extra";
 var env = nunjucks.configure();
 
 let startsWith = (str, targetStr) => {
-    console.log("str, targetStr : ", str, targetStr);
+    // console.log("str, targetStr : ", str, targetStr);
     if (!str.length) return false
     return str.startsWith(targetStr)
 };
 
 env.addGlobal('startsWith', startsWith);
 
-let html= fs.readFileSync("Normal.html", 'utf8');
+let html = fs.readFileSync("Normal.html", 'utf8');
 
 sidebar.forEach(sidebarItem => {
     if ("children" in sidebarItem) {
         sidebarItem.children.forEach(child => {
-            if("template" in child) {
-                html = fs.readFileSync(`Template/${child.template}`, 'utf8');
-            }
-            else{
-                html = fs.readFileSync("Normal.html", 'utf8');
-            }
+            let LocalFileName = ("template" in child) ? child.template : "Normal.html";
+            let html = fs.readFileSync(`Template/${LocalFileName}`, 'utf8');
 
             let data = nunjucks.renderString(html,
                 { sidebar, sidebarItem: child }
             );
-            let folder = child.url.slice(0,-5);
-            fs.mkdirSync(`bin/${folder}`);
+            let folder = child.url.slice(0, -5);
+
+            if (!fs.existsSync(`bin/${folder}`)) fs.mkdirSync(`bin/${folder}`);
             fs.writeFileSync(`bin/${folder}/${child.url}`, data);
         });
     }
     else {
-        if("template" in sidebarItem) {
+        if ("template" in sidebarItem) {
             html = fs.readFileSync(`Template/${sidebarItem.template}`, 'utf8');
         }
-        else{
+        else {
             html = fs.readFileSync("Normal.html", 'utf8');
-        }
+        };
+
         let data = nunjucks.renderString(html,
             { sidebar, sidebarItem }
         );
-        let folder = sidebarItem.url.slice(0,-5);
-        fs.mkdirSync(`bin/${folder}`);
+        let folder = sidebarItem.url.slice(0, -5);
+
+        if (!fs.existsSync(`bin/${folder}`)) fs.mkdirSync(`bin/${folder}`);
+        // fs.mkdirSync(`bin/${folder}`);
         fs.writeFileSync(`bin/${folder}/${sidebarItem.url}`, data);
-    }
+    };
 });
 
 // console.log(sidebar);
