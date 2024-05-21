e42fde8f65cf3651d16cb01dea2e86b01a565996
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-05-21 17:37:43 +0530

    keshav

--- Quotations/EntryFile.js
+++ Quotations/EntryFile.js
@@ -1,30 +1,33 @@
-import sidebar from './sidebar.json' assert { type : "json"};
+import sidebar from './sidebar.json' assert { type: "json"};
 import nunjucks from "nunjucks";
 import fs from "fs-extra";
 var env = nunjucks.configure();
+
 let startsWith = (str, targetStr) => {
+    console.log("str, targetStr : ", str, targetStr);
     if (!str.length) return false
     return str.startsWith(targetStr)
-}
+};
+
 env.addGlobal('startsWith', startsWith);
 
 const html = fs.readFileSync("Normal.html", 'utf8');
 
 sidebar.forEach(sidebarItem => {
-    if("children" in sidebarItem){
+    if ("children" in sidebarItem) {
         sidebarItem.children.forEach(child => {
             let data = nunjucks.renderString(html,
-                {sidebar, sidebarItem : child}
+                { sidebar, sidebarItem: child }
             );
             fs.writeFileSync(`bin/${child.url}`, data);
         });
     }
     else {
         let data = nunjucks.renderString(html,
-            {sidebar, sidebarItem }
+            { sidebar, sidebarItem }
         );
         fs.writeFileSync(`bin/${sidebarItem.url}`, data);
     }
 });
 
-console.log(sidebar);
+// console.log(sidebar);
