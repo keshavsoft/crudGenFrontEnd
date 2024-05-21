37bef5276921d38a7812166dddce58cd23b6b68f
Ch-Sai-Jaswanth <saijaswanthch2003@gmail.com>
2024-05-21 17:00:49 +0530

    Added Quotations

---
+++ Quotations/EntryFile.js
@@ -0,0 +1,30 @@
+import sidebar from './sidebar.json' assert { type : "json"};
+import nunjucks from "nunjucks";
+import fs from "fs-extra";
+var env = nunjucks.configure();
+let startsWith = (str, targetStr) => {
+    if (!str.length) return false
+    return str.startsWith(targetStr)
+}
+env.addGlobal('startsWith', startsWith);
+
+const html = fs.readFileSync("Normal.html", 'utf8');
+
+sidebar.forEach(sidebarItem => {
+    if("children" in sidebarItem){
+        sidebarItem.children.forEach(child => {
+            let data = nunjucks.renderString(html,
+                {sidebar, sidebarItem : child}
+            );
+            fs.writeFileSync(`bin/${child.url}`, data);
+        });
+    }
+    else {
+        let data = nunjucks.renderString(html,
+            {sidebar, sidebarItem }
+        );
+        fs.writeFileSync(`bin/${sidebarItem.url}`, data);
+    }
+});
+
+console.log(sidebar);
