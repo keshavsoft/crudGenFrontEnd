93bccec41335b63eac9c69f60e3487dd723c2c0c
SandhyaAvula363 <avulavenkatasandhya@gmail.com>
2024-05-28 13:34:04 +0530

    added last items create

--- public/Garments/Stiching/Create/Js/FormLoad/EntryFile.js
+++
@@ -1,8 +0,0 @@
-import { StartFunc as StartFuncForAccountNames } from "./ForDataList/AccountNames/GetFetch.js";
-import {StartFunc as StartFuncForSuggestedItems} from "./SuggestedItems.js"
-const StartFunc = () => {
-    StartFuncForAccountNames();
-    StartFuncForSuggestedItems();
-};
-
-export { StartFunc };
--- public/Garments/Stiching/Voucher/Js/Addlisteners/SaveButtonClickId/FetchFromFuncs/AfterFetch/EntryFile.js
+++ public/Garments/Stiching/Voucher/Js/Addlisteners/SaveButtonClickId/FetchFromFuncs/AfterFetch/EntryFile.js
@@ -2,7 +2,7 @@ let StartFunc = ({ InData }) => {
     let JVarLocal = InData.pk;
     const url = new URL(window.location.href);
     const params1 = new URLSearchParams(url.search);
-    let NewURl = new URL("../VoucherItems/ShowAll.html", url);
+    let NewURl = new URL("../LastItemsCreate/Create.html", url);
     const new_url = new URL(`${NewURl.href}?${params1}`);
     new_url.searchParams.append('StichRef', JVarLocal);
 
