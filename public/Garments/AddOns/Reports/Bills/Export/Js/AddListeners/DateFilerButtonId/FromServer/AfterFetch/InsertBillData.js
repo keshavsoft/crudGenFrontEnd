024b90db8d9a362906208eec4710e65a4a1d2407
Benjemin Caleb <benjemin15caleb@gmail.com>
2024-06-25 18:11:12 +0530

    added export in reports showall

---
+++ public/Garments/AddOns/Reports/Bills/Export/Js/AddListeners/DateFilerButtonId/FromServer/AfterFetch/InsertBillData.js
@@ -0,0 +1,23 @@
+let StartFunc = ({ inItemDetails, inBillData }) => {
+    let jVarLocalItemDetails = inItemDetails;
+    let jVarLocalBillData = inBillData;
+
+    let jVarLocalClubbedData = jVarLocalItemDetails.map(element => {
+        let LoopInsideFind = jVarLocalBillData.find(LoopMaster => {
+            return LoopMaster.pk === parseInt(element.BillPk);
+        });
+
+        if (LoopInsideFind === undefined === false) {
+            return {
+                ...element,
+                CustomerNumber: LoopInsideFind?.CustomerNumber,
+                Date: LoopInsideFind?.Date,
+                BillNumber: LoopInsideFind?.BillNumber2425,
+                CustomerName: LoopInsideFind?.CustomerName
+            };
+        };
+    });
+
+    return jVarLocalClubbedData;
+};
+export { StartFunc }
